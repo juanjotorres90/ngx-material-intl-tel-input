33383775ffa477b7e63e3564fade6bdf5759755b
Juanjo Torres <juanjotorres90@gmail.com>
2025-07-11 14:47:44 +0200

    chore: :fire: remove jest.dev.config.js and related settings from VSCode configuration

--- jest.dev.config.js
+++
@@ -1,7 +0,0 @@
-const preset = require('./jest.preset.js');
-
-module.exports = {
-  ...preset,
-  collectCoverage: false,
-  coverageReporters: undefined // Remove coverage reporters to speed up
-};
